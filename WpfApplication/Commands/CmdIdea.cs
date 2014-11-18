91fc468f010cf5186f4f7f33b8a973fccc487d13
Andrew Seward <awswrd@gmail.com>
2014-11-18 12:09:38 -0800

    Think CmdIdea is in roughly the right direction

---
+++ WpfApplication/Commands/CmdIdea.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication.Commands
+{
+    public abstract class CmdIdea : ICommand
+    {
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public virtual void Execute(object parameter)
+        {
+            if (!SetUpSteps(parameter)) { return; }
+
+            try
+            {
+                _execution();
+            }
+            catch (Exception ex)
+            {
+                _error(ex);
+            }
+            finally
+            {
+                _ensure();
+            }
+        }
+
+        protected abstract bool SetUpSteps(object parameter);
+
+        protected Action<Exception> _error = (ex) => { };
+
+        protected Action _execution = () => { };
+
+        protected Action _ensure = () => { };
+    }
+
+    public abstract class CmdIdea<T> : CmdIdea where T : ViewModel
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return base.CanExecute(parameter)
+                && parameter != null
+                && parameter is T;
+        }
+
+        protected override bool SetUpSteps(object parameter)
+        {
+            if (parameter is T)
+            {
+                var viewModel = (T) parameter;
+                return SetUpSteps(viewModel);
+            }
+
+            return false;
+        }
+
+        protected abstract bool SetUpSteps(T viewModel);
+    }
+}
